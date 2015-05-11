492ec3047a919326f6313a6cc0e081f8bc91f433
bregan84 <patricklkw@gmail.com>
2015-05-11 13:47:21 +0800

    update temp path

--- test.ps1
+++ test.ps1
@@ -1,5 +1,5 @@
 $x=0
-Invoke-WebRequest "https://download.sysinternals.com/files/SysinternalsSuite.zip" -OutFile "H:\My Music\a.zip"
-Invoke-WebRequest "http://the.earth.li/~sgtatham/putty/latest/x86/putty.zip" -OutFile "H:\My Music\b.zip"
+Invoke-WebRequest "https://download.sysinternals.com/files/SysinternalsSuite.zip" -OutFile "$env:TEMP\a.zip"
+Invoke-WebRequest "http://the.earth.li/~sgtatham/putty/latest/x86/putty.zip" -OutFile "$env:TEMP\b.zip"
 while ($x -le 30)
 {$urls=Get-Random -input "yahoo.com","grifsec.com","microsoft.com","google.com";[System.Net.Dns]::GetHostAddresses("$urls");Start-Sleep -s 1;$x++}
